f632380163be5a613e1d703ab9c2ac1f7aa49dd6
MinhajSadik <MinhajoSadik@gmail.com>
2021-08-18 19:42:55 +0600

    [add] some requirements

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const expressLayout = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-// const err = new MongooseError(message);
 const { MongoClient } = require('mongodb');
 
 
@@ -18,16 +17,14 @@ const { MongoClient } = require('mongodb');
 // });
 
 const uri = "mongodb+srv://MinhajSadik:MongoDB1@cluster0.4y50m.mongodb.net/pizzaApp?retryWrites=true&w=majority";
-mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true});
 
+mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error:'));
 connection.once('open', () => {
   // we're connected!
   console.log('connected')
 });
-
-
-
 
 
 //Assets
