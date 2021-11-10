df8004d3b7efc9a64b55855547de8fe87aae6234
MinhajSadik <MinhajoSadik@gmail.com>
2021-11-11 02:33:59 +0600

    [ADD] Done Cart Page Working

---
+++ resources/js/app.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import Noty from "noty";
+let addToCart = document.querySelectorAll(".add-to-cart");
+
+let cartCounter = document.querySelector("#cartCounter");
+
+async function updateCart(pizza) {
+  await axios
+    .post("/update-cart", pizza)
+    .then((res) => {
+      console.log(res);
+      cartCounter.innerText = res.data.totalQty;
+      new Noty({
+        type: "success",
+        text: "Item Added to Cart",
+        timeout: 1000,
+        progressBar: false,
+        layout: "topRight",
+      }).show();
+    })
+    .catch((err) => {
+      new Noty({
+        type: "error",
+        text: "Something Went Wrong!",
+        timeout: 1000,
+        progressBar: false,
+        layout: "topLeft",
+      }).show();
+    });
+}
+
+addToCart.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let pizza = JSON.parse(btn.dataset.pizza);
+    updateCart(pizza);
+  });
+});
--- server.js
+++ server.js
@@ -1,37 +1,69 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const path = require('path');
-const expressLayout = require('express-ejs-layouts');
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3000;
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo")(session);
 
-app.use(cors());
-app.use(bodyParser.json({extended: true}));
-app.use(bodyParser.urlencoded({extended: true}));
+//Database Connection
+const url = "mongodb://localhost:27017/pizza";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
 
+const connection = mongoose.connection;
+
+connection
+  .once("open", () => {
+    console.log("Database Connected...");
+  })
+  .catch((err) => {
+    console.log("Database Connection Faild...");
+  });
+
+//Session Store
+let mongoStore = new MongoDbStore({
+  mongooseConnection: connection,
+  collection: "sessions",
+});
+
+//Session Config
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: mongoStore,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // for 24 hours
+    // cookie: { maxAge: 1000 * 10 }, //for 10 sec
+  })
+);
+
+app.use(flash());
 
 //Assets
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.json());
+
+//Global Middleware
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
 
 // set template engine
 app.use(expressLayout);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
+require("./routes/web")(app);
 
-require('./routes/web')(app);
-
-
-const uri = "mongodb+srv://Realtime-Pizza-App:realtime-pizza-app@cluster0.4y50m.mongodb.net/pizzaApp?retryWrites=true&w=majority";
-
-mongoose.connect(uri,{useNewUrlParser: true,  useUnifiedTopology: true})
-.then(()=> app.listen(PORT, () => console.log(`Listing on Port ${PORT}`)))
-.catch((error) => console.log(error.message));
-
-mongoose.set('useFindAndModify', false);
-
-
-
+app.listen(PORT, () => console.log(`Listing on Port ${PORT}`));
